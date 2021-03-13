370fa0585e7a22dd5775f613afb0564c7c4f98d8
kevenescovedo <49500205+kevenescovedo@users.noreply.github.com>
2021-03-13 11:45:55 -0300

    Add files via upload

--- criar_excluir_bd.sql
+++ criar_excluir_bd.sql
@@ -2,3 +2,7 @@
 create DATABASE bd_curso_web;
 //remover um banco de dados
 DROP DATABASE bd_curso_web;
+//criar uma tabela;
+CREATE TABLE tb_cursos (id_curso INT NOT NULL, image_curso VARCHAR(100) NOT NULL, nome_curso CHAR(50) NOT NULL, resumo_curso TEXT NULL, data_cadastro DATETIME NOT NULL, ativo boolean DEFAULT true, investimento FLOAT(8,2) DEFAULT 0 );
+/excluindo tabelas
+DROP TABLE tb_cursos;
